674f59bccf26b6923e701eb6b6cfea6ef8d44ac7
lanrefahm <lanrefahm@gmail.com>
2022-09-27 14:56:10 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * set_string - sets the value of apointer to a char
+ * set_string - sets the value of a pointer to a char
  * @s: pointer to pointer we need to set to
  * @to: string to set
  */
